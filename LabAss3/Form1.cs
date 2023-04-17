31765906aff00ca16ef1a4f9d21225b1ea27b097
0310LiuYifeng <127761188+lyf193615@users.noreply.github.com>
2023-04-17 17:25:33 +0800

    LabAss3

---
+++ LabAss3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace LabAss3
+{
+    public partial class frmCustomerDataEntry : Form
+    {
+        public frmCustomerDataEntry()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Gender, Hobby, Status = "";
+
+            if (radioMale.Checked) Gender = "Male";
+            else Gender = "Female";
+            if (chkReading.Checked) Hobby = "Reading";
+            else Hobby = "Painting";
+            if (radioMarried.Checked) Status = "Married";
+            else Status = "Unmarried";
+
+            try
+            {
+                CustomerValidation objVal= new CustomerValidation();
+                objVal.CheckCustomerName(txtName.Text);
+
+                frmCustomerPreview objPreview = new frmCustomerPreview();
+                objPreview.SetValues(txtName.Text, cmbCountry.Text, Gender, Hobby, Status);
+            } 
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
